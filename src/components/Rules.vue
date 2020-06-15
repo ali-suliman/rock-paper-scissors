832234430f836acd953d9813bbca053409bea498
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-06-15 12:06:07 +0200

    Create Rules.vue

---
+++ src/components/Rules.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container">
+    <div class="inner-container">
+      <section class="head">
+        <h3>rules</h3>
+        <span @click="hideRules">X</span>
+      </section>
+      <img src="../assets/images/image-rules.svg" alt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Rules",
+  methods: {
+    hideRules: function() {
+      this.$emit("hideRules");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  background: #000a;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .inner-container {
+    background: #fff;
+    padding: 1rem 2.6rem;
+    border-radius: 10px;
+
+    .head {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0.4rem 0rem;
+      margin: 1rem 0rem;
+
+      h3 {
+        text-transform: uppercase;
+      }
+
+      span {
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+@media only screen and (max-width: 380px) {
+  .container {
+    background: #fff;
+
+    .inner-container {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: center;
+
+      .head {
+        width: 100%;
+      }
+
+      img {
+        margin-bottom: 8rem;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/Scoreboard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="scoreboard">
+    <img src="../assets/images/logo.svg" alt="Rock, Paper, Scissors" />
+    <article class="score">
+      <p>score</p>
+      <span id="score-val">{{ score }}</span>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    score() {
+      return this.$store.state.score;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.scoreboard {
+  width: 90%;
+  max-width: 40rem;
+  min-height: 6rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem 1rem;
+  border-radius: 10px;
+  border: solid 3px #fff4;
+
+  img {
+    width: 5.8rem;
+  }
+  .score {
+    background: linear-gradient(#fff, #cfcfcf);
+    box-shadow: #0004 0px 0px 16px;
+    height: 5rem;
+    width: 5.4rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 5px;
+    padding: 0.6rem;
+
+    p {
+      color: #1e34a1;
+      opacity: 0.8;
+      font-weight: 600;
+    }
+
+    span {
+      color: #565368;
+      font-size: 2.6rem;
+      font-weight: 700;
+    }
+  }
+}
+</style>
