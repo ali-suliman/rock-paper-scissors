dcd75cb1a3c1407a5290c648e3914721737ba180
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-06-15 12:06:24 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SelectionBoard />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SelectionBoard from "../components/SelectionMenu.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    SelectionBoard
   }
-}
+};
 </script>
+
+<style lang="scss">
+.home {
+  width: 100%;
+  height: 30rem;
+  background: #1a2749;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
